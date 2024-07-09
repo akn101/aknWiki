2492d8870430b9454ecac7fa921b462edbd203a8
Ahnaf Kabir <kabir.a@akn.me.uk>
2024-07-10 00:22:02 +0100

    Initialise

---
+++ build/assets/js/36994c47.e1a68d72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkakn_wiki=self.webpackChunkakn_wiki||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ build/assets/js/acecf23e.6f64c0ab.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkakn_wiki=self.webpackChunkakn_wiki||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
